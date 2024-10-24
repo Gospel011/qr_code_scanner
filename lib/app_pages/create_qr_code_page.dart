21e5126aeab46b463792680f48125e65e74493a8
Gospel Ugochukwu <Ugochukwu Gospel>
2024-10-24 17:17:44 +0100

    Saved

--- lib/app_pages/create_qr_code_page.dart
+++ lib/app_pages/create_qr_code_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:qr_flutter/qr_flutter.dart';
 
 class CreateQrCodePage extends StatefulWidget {
   const CreateQrCodePage({super.key});
@@ -11,9 +11,21 @@ class CreateQrCodePage extends StatefulWidget {
 class _CreateQrCodePageState extends State<CreateQrCodePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Create QR code page.'),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white),
+          child: QrImageView(
+            data: 'This QR code has an embedded image as well',
+            version: QrVersions.auto,
+            size: 320,
+            gapless: false,
+            embeddedImage: AssetImage('assets/app_icon/app_icon.png'),
+            embeddedImageStyle: QrEmbeddedImageStyle(
+              size: Size(40, 40),
+            ),
+          ),
+        ),
       ),
     );
   }
